6cf334320b7540c52c8f8071a54339665aef02dd
jandrogarciagarcia <jandrogarciagarcia@gmail.com>
2025-05-03 11:45:41 +0200

    fix(minio): typo in MinIO utils solved.

--- src/app/ai/model_ml.py
+++ src/app/ai/model_ml.py
@@ -1,26 +1,23 @@
 import json
 from typing import List, Optional
-import cv2
 import numpy as np
 import torch
 from loguru import logger
 from mlserver import MLModel
 from mlserver.codecs import decode_args
-from mlserver.utils import get_model_uri
 from PIL import Image
-from diffusers import StableDiffusionControlNetImg2ImgPipeline, ControlNetModel, UniPCMultistepScheduler
+from diffusers import (
+    StableDiffusionControlNetImg2ImgPipeline,
+    ControlNetModel,
+)
 from transformers import pipeline
 from src.app.utils.image_utils import get_depth_map
-from src.app.utils.minio_utils import get_s3, retrieve_files, upload_images
+from src.app.utils.minio_utils import get_s3, retrieve_file, upload_images
 from src.app.config.config import settings
 
 
 class Model(MLModel):
-
-    async def process_images(
-        self,
-        image: dict[str, np.array]
-    ) -> torch.Tensor:
+    async def process_images(self, image: dict[str, np.array]) -> torch.Tensor:
         """
         Preprocess the image to be used by the model.
         :param image: Image in cache to process
@@ -31,10 +28,12 @@ class Model(MLModel):
         image = list(image.values())[0]
 
         # Convert to PIL Image
-        img = Image.fromarray(np.uint8(image)).convert('RGB')
+        img = Image.fromarray(np.uint8(image)).convert("RGB")
 
         # Resize to lower resolution
-        img = img.resize((img.size[0] // 3, img.size[1] // 3), resample=Image.Resampling.LANCZOS)
+        img = img.resize(
+            (img.size[0] // 3, img.size[1] // 3), resample=Image.Resampling.LANCZOS
+        )
 
         # Compute the depth map
         depth_map = get_depth_map(img, self._estimator).unsqueeze(0).half().to("cuda")
@@ -57,18 +56,18 @@ class Model(MLModel):
             "lllyasviel/control_v11f1p_sd15_depth",
             torch_dtype=torch.float16,
             use_safetensors=True,
-            cache_dir="./model_cache"
+            cache_dir="./model_cache",
         )
 
         logger.info("Loading Stable Difussion...")
 
         # Pipeline for Stable Diffusion with ControlNet
         self._predictive = StableDiffusionControlNetImg2ImgPipeline.from_pretrained(
-        "runwayml/stable-diffusion-v1-5",
-        controlnet=self._controlnet,
-        torch_dtype=torch.float16,
-        use_safetensors=True,
-        cache_dir="./model_cache"
+            "runwayml/stable-diffusion-v1-5",
+            controlnet=self._controlnet,
+            torch_dtype=torch.float16,
+            use_safetensors=True,
+            cache_dir="./model_cache",
         )
 
         # Estimator for depth estimation
@@ -96,10 +95,10 @@ class Model(MLModel):
         """
 
         # Search for the files in the S3 bucket
-        dict_images = retrieve_files(s3=self._s3_client, imgs_path=image_path[0])
+        dict_images = retrieve_file(s3=self._s3_client, img_path=image_path[0])
 
         if not dict_images:
-            logger.error('No file paths found in the S3 bucket')
+            logger.error("No file paths found in the S3 bucket")
             return [
                 f'{{"message": {json.dumps("Wrong path to the images in the S3 bucket")}}}'
             ]
@@ -116,9 +115,16 @@ class Model(MLModel):
                 guidance_scale=7.5,
             ).images[0]
 
-
             # Upload the results to the S3 bucket
-            upload_images(s3=self._s3_client,  minio_path=settings.s3.folder + '/' + image_path[0] + "/" + settings.s3.result_folder, images={image_path[0]: output})
+            upload_images(
+                s3=self._s3_client,
+                minio_path=settings.s3.folder
+                + "/"
+                + image_path[0]
+                + "/"
+                + settings.s3.result_folder,
+                images={image_path[0]: output},
+            )
 
             return [
                 f'{{"result_minio_path": "{settings.s3.out_path}","images_path": {json.dumps(list(dict_images.keys()))}, "message": {json.dumps("Success")}}}'
