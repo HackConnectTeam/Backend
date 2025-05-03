5373bdaa77755fd51aeced29bbe083d79536926e
jandrogarciagarcia <jandrogarciagarcia@gmail.com>
2025-05-03 10:45:34 +0200

    fix: little typo fixed.

--- src/app/utils/api_utils.py
+++ src/app/utils/api_utils.py
@@ -5,8 +5,10 @@ import requests
 from loguru import logger
 
 
-def build_inference_url(model_settings_path: str = "config/model_config/model-settings.json",
-                        settings_path: str = "config/settings.json") -> str:
+def build_inference_url(
+    model_settings_path: str = "config/model_config/model-settings.json",
+    settings_path: str = "config/settings.json",
+) -> str:
     """
     Build the URL for the inference endpoint.
     :param model_settings_path: Path to the model settings file.
@@ -18,39 +20,35 @@ def build_inference_url(model_settings_path: str = "config/model_config/model-se
     settings = json.load(open(settings_path))
 
     # Extract the model name, host and port
-    model_name= model_settings['name']
-    host = settings['host']
-    port = settings['http_port']
+    model_name = model_settings["name"]
+    host = settings["host"]
 
-    return f"http://{host}:{port}/v2/models/{model_name}/infer"
+    return f"http://{host}/v2/models/{model_name}/infer"
 
-def inference_task(user_id: str,
-                   image_path: str):
+
+def inference_task(user_id: str, image_path: str):
     """
     Perform inference on the provided images and parcel ID.
     :param user: ID of the user
     :param image_path: Path of the folder with image
     :return:
     """
-    # Build the inference URL where the model is deployed
+    # Build the inference URL where the model is dettingeployed
     inference_url = build_inference_url()
 
     if not user_id or not image_path:
-        raise HTTPException(status_code=400, detail="parcel_id and image_paths cannot be empty")
+        raise HTTPException(
+            status_code=400, detail="parcel_id and image_paths cannot be empty"
+        )
 
     data = {
         "inputs": [
-            {
-                "name": "user_id",
-                "shape": [1],
-                "datatype": "BYTES",
-                "data": [user_id]
-            },
+            {"name": "user_id", "shape": [1], "datatype": "BYTES", "data": [user_id]},
             {
                 "name": "image_path",
                 "shape": [1],
                 "datatype": "BYTES",
-                "data": [image_path]
+                "data": [image_path],
             },
         ]
     }
@@ -60,9 +58,10 @@ def inference_task(user_id: str,
     response = requests.post(inference_url, json=data)
 
     if response.status_code != 200:
-        raise HTTPException(status_code=response.status_code,
-                            detail="Unknown error during inference request")
+        raise HTTPException(
+            status_code=response.status_code,
+            detail="Unknown error during inference request",
+        )
 
     response = response.json()
     print(response)
-
