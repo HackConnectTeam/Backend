c8a5a77fa573866a470562ea56e050d8de9bff2f
jandrogarciagarcia <jandrogarciagarcia@gmail.com>
2025-05-03 11:13:57 +0200

    fix: another little typo fixed.

--- src/app/routers/model.py
+++ src/app/routers/model.py
@@ -1,9 +1,10 @@
-from fastapi import APIRouter, HTTPException, BackgroundTasks, Body, Depends
+from fastapi import APIRouter, BackgroundTasks, Body
 from src.app.utils.api_utils import inference_task
 
 from src.app.models.prediction_response import PredictionResponse
 
 router = APIRouter(prefix="/ToMii", tags=["To Mii"])
+
 
 @router.post(
     "/",
@@ -12,15 +13,15 @@ router = APIRouter(prefix="/ToMii", tags=["To Mii"])
 )
 async def predict(
     background_tasks: BackgroundTasks,
-    user_id: str = Body(..., description='Unique user id'),
-    image_path: str = Body(..., description='Path (from the bucket) to the image for inference'),
+    user_id: str = Body(..., description="Unique user id"),
+    image_path: str = Body(
+        ..., description="Path (from the bucket) to the image for inference"
+    ),
 ):
-
-
     # Call the inference function asynchronously
-    background_tasks.add_task(inference_task, user_id, image_path)
+    background_tasks.add_task(inference_task, [user_id], [image_path])
 
     return {
         "status": "success",
-        "message": f"Prediction for user: {user_id} added to the queue."
+        "message": f"Prediction for user: {user_id} added to the queue.",
     }
