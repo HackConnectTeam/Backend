b3f0353885b338a2d34fe63a356aeca18beae012
AlejandroDopico2 <alejandro.dopico2@udc.es>
2025-05-03 12:58:59 +0200

    feat: Get Countries

---
+++ src/app/routers/country.py
@@ -0,0 +1,17 @@
+from typing import List
+from fastapi import APIRouter
+import pycountry
+
+from src.app.schemas.country import Country
+
+router = APIRouter()
+
+
+@router.get("/countries", response_model=List[Country])
+def get_countries():
+    countries = [
+        {"code": country.alpha_2, "name": country.name}
+        for country in pycountry.countries
+    ]
+    countries.sort(key=lambda x: x["name"])
+    return countries
